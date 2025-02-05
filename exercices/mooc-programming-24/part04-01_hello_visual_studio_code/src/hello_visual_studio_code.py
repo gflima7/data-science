a3d5d88d4d48960535a1451eca98b7d8e220509a
Gabriel Lima <81051291+gflima7@users.noreply.github.com>
2025-02-05 11:09:09 +0100

    ex1

--- exercices/mooc-programming-24/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ exercices/mooc-programming-24/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,11 @@
 # Write your solution here
+while True:
+    editor = input("Editor: ").lower()  # Convert input to lowercase
+    
+    if editor == "visual studio code":
+        print("an excellent choice!")
+        break  # Exit loop when the correct editor is entered
+    elif editor in ["word", "notepad"]:
+        print("awful")
+    else:
+        print("not good")
